80f36cc839cc81d9017f9039a0c7605a99b07e65
Justine C <83780776+JordyKonpyuta@users.noreply.github.com>
2024-11-25 08:36:29 +0100

    Import assets for ennemies

--- Source/TowerDefense/Public/Ennemy.h
+++ Source/TowerDefense/Public/Ennemy.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Character.h"
 #include "Ennemy.generated.h"
 
+UENUM(BlueprintType)
+enum class EEnnemyType : uint8
+{
+	Wolf UMETA(DisplayName = "Wolf"),
+	Bear UMETA(DisplayName = "Bear"),
+	Bat UMETA(DisplayName = "Bat"),
+	Chicken UMETA(DisplayName = "Chicken"),
+};
+
 UCLASS()
 class TOWERDEFENSE_API AEnnemy : public ACharacter
 {
@@ -22,8 +31,14 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void OnDeath();
+
+	UFUNCTION()
+	void InitEnnemy();
 
 };
